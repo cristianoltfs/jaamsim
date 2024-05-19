63d391928d77773f70937f3311413579c05be512
cristianoltfs <cristianoltfs@gmail.com>
2024-05-19 13:33:43 -0300

    fila única vs fila múltipla

---
+++ 007_fila_unica_vs_multipla/fila.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  5 20:42:22 2024
+@author: cristianoltfs
+"""
+
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default="browser"
+
+fila = pd.read_csv("fila.csv", sep = ";")
+fila.head()
+
+fila = pd.melt(fila, id_vars='Chegada', value_vars=['AverageQueueTimeUnicaMin','AverageQueueTimeMultiplaMin', 'DiferencaTime', 'MaxEntityWaitingUnica', 'MaxEntityWaitingMultipla', 'DiferencaWait'])
+
+
+gfila = px.bar(fila,
+             x = "value",
+             y = "Chegada",
+             color = "variable",
+             barmode = "group"
+)
+
+gfila.update_yaxes(title = "Intervalo entre chegada")
+
+gfila.update_xaxes(title = "Valor")
+
+
+gfila.show()
+
+gfila.write_html("fila.html")
